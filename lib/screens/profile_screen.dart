dbadf1b3f6f233ba7f451cd399a4177fe626b73c
JMichaelYang <jaewonmichael@gmail.com>
2018-04-16 11:42:00 -0400

    Updated profile to stateless

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:kickit/models/kickit.dart';
 import 'package:kickit/utils/values/keys.dart';
 
-/// Screen that display's the user's [Profile] information as well as history.
-class ProfileScreen extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return new _ProfileScreenState();
-  }
-}
-
 /// Handles the state for a [ProfileScreen].
-class _ProfileScreenState extends State<ProfileScreen> {
+class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    KickItState state = KickIt.of(context);
+
     return new Scaffold(
       key: Keys.profileScaffoldKey,
     );
