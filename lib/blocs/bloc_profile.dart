645cfc6ff045cc26f5c7fd4b9ec195e874e29188
JMichaelYang <jaewonmichael@gmail.com>
2018-10-23 12:07:46 -0400

    Example

--- lib/blocs/bloc_profile.dart
+++ lib/blocs/bloc_profile.dart
@@ -2,7 +2,7 @@ import 'package:kickit/blocs/bloc_provider.dart';
 
 /// A BLoC that handles the manipulation and retrieving of [Profile] data.
 class BlocProfile extends BlocBase {
-
+  
 
   @override
   void dispose() {
